027fed8712f4e32a0e8a7e0fb319242f56e93f9b
Jelle Sjollema <=>
2019-02-21 13:16:33 +0100

    images added

--- js/Student.js
+++
@@ -1,7 +0,0 @@
-let mijnMa = {};
-
-mijnMa.student = {};
-
-mijnMa.student.uur=0;
-mijnMa.student.dag=0;
-mijnMa.student.punten =0;
--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const myPossebilities = document.getElementById('possebilities');
 const myButton = document.getElementById('myButton');
 const myInput = document.getElementById('myInput');
 const feedback = document.getElementById('feedback');
+const imageLocation = document.getElementById('imageLocation');
 
 let currentLocation = 4;
 
@@ -17,6 +18,17 @@ locations[6] = "lokatie 6";
 locations[7] = "lokatie 7";
 locations[8] = "lokatie 8";
 
+images = [];
+images[0] = "room0.png";
+images[1] = "room1.png";
+images[2] = "room2.png";
+images[3] = "room3.png";
+images[4] = "room4.png";
+images[5] = "room5.png";
+images[6] = "room6.png";
+images[7] = "room7.png";
+images[8] = "room8.png";
+
 directions = [];
 directions[0] = ["oost"];
 directions[1]= ["west","zuid"];
@@ -24,9 +36,9 @@ directions[2]= ["zuid"];
 directions[3]= ["oost"];
 directions[4]= ["noord","west","zuid"];
 directions[5]= ["zuid"];
-directions[6]= ["west"];
+directions[6]= ["oost"];
 directions[7]= ["noord","west","oost"];
-directions[8]= ["noord","oost"];
+directions[8]= ["noord","west"];
 
 myInput.addEventListener('keydown',getInput,false);
 
@@ -45,10 +57,10 @@ function getInput(evt){
           currentLocation += 3;
         break;
         case "oost":
-          currentLocation -= 1;
+          currentLocation += 1;
         break;
         case "west":
-          currentLocation += 1;
+          currentLocation -= 1;
         break;
       }
     } else {
@@ -68,7 +80,8 @@ function getInput(evt){
 
 function giveLocation(){
   divLocation.innerHTML = locations[currentLocation];
-  myDirections = "";
+  imageLocation.src = "media/" + images[currentLocation];
+  myDirections = "mogelijke richtingen zijn: ";
   for(let i = 0; i< directions[currentLocation].length;i++){
     myDirections += directions[currentLocation][i] + " ";
   }
